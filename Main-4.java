f78d586de789ac25c272b38452a83a0786ee0283
wilsonlaww <69646455+wilsonlaww@users.noreply.github.com>
2020-08-17 12:13:17 -0700

    Add files via upload

---
+++ Main-4.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		
+	    Scanner reader = new Scanner(System.in);
+	    System.out.println("Enter distance to tartget.\n");
+        double d = reader.nextDouble();
+        System.out.println("Enter the height of the battery relative to the water line.");
+        double h = reader.nextDouble();
+        System.out.println("Enter the initial velocity of the shell.\n");
+        double v = reader.nextDouble();
+        
+        double a = -4.9*(d/v)*(d/v);
+        double b = d;
+        double c = a +h;
+        
+        double tanTheta = (-b + Math.sqrt(b*b - 4*a*c))/(2*a);
+        double angle = Math.atan(tanTheta)*180/3.1415926;
+        System.out.println("The angle is " + angle);
+        
+        
+	}
+    }
+    
+        
